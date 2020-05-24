78ddf85c6a0bd28e5962f797385b6004a767d78f
Liza <bliza0812@gmail.com>
2020-05-24 19:02:22 +0400

    Answer

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        int arraySize = 3;
+        String[] array = new String[arraySize];
+        String element;
+        int curPos = 0;
+        Scanner scanner = new Scanner(System.in);
+        element = scanner.nextLine();
+
+        while (!element.equals("0")) {
+            array[curPos] = element;
+            if (curPos == array.length - 1) {
+                arraySize += arraySize;
+                String[] arrayNew = new String[arraySize];
+                for (int i = 0; i < array.length; i++) {
+                    arrayNew[i] = array[i];
+                }
+                array = arrayNew;
+            }
+            curPos++;
+            element = scanner.nextLine();
+        }
+
+        System.out.println();
+        for(int i = 0; i < curPos; i++){
+            System.out.println(array[i]);
+        }
     }
 }
